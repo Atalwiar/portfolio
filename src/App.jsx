90e6b199b6c161b3eca4525b2fdab5b34ae403b5
Atalwiar077 <Atalwiar145@gmail.com>
2025-11-07 17:19:21 +0430

    Initial commit - React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold underline">
+        Tailwind + React + Vite ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
